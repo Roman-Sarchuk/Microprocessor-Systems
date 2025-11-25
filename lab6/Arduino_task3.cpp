91be5a7b54b18a6d5514a84b16785ab13afd96b9
Roman-Sarchuk <91730539+Roman-Sarchuk@users.noreply.github.com>
2025-11-25 14:49:11 +0200

    Complete lab6-ard-t3

---
+++ lab6/Arduino_task3.cpp
@@ -0,0 +1,40 @@
+#include <LiquidCrystal.h>
+
+const int RS = 8;
+const int E = 9;
+const int D4 = 4;
+const int D5 = 5;
+const int D6 = 6;
+const int D7 = 7;
+const int BtnPin = 2;
+
+LiquidCrystal lcd(RS, E, D4, D5, D6, D7);
+bool lastButtonState = HIGH;
+bool buttonState = HIGH;
+
+
+void printRandNum() {
+  lcd.setCursor(0, 0);
+  lcd.print("                ");
+  lcd.setCursor(0, 0);
+  lcd.print(random());
+}
+
+
+void setup() {
+  lcd.begin(16, 2);
+
+  pinMode(BtnPin, INPUT_PULLUP);
+
+  printRandNum();
+}
+
+void loop() {
+  buttonState = digitalRead(BtnPin);
+
+  if (lastButtonState == HIGH && buttonState == LOW) {
+    printRandNum();
+  }
+
+  lastButtonState = buttonState;
+}
